7f02b094ab691e5058e59512c549a4bca271dfe6
Beam27 <nakosh0003@gmail.com>
2022-04-30 18:11:43 +0600

    part#2 log in

--- Web/Web/settings.py
+++ Web/Web/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'login.apps.LoginConfig',
 
+
 ]
 
 MIDDLEWARE = [
--- Web/login/urls.py
+++ Web/login/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/sign_in', views.sing_in, name='sign_in'),
-    path('/log_in', views.log_in, name='log_in'),
+    path('sign_in', views.sing_in, name='sign_in'),
+    path('log_in', views.log_in, name='log_in'),
 ]
