41a86f2bb91a3c21dbeab3303fa432d94f7c158e
200103032 <issadyk003@gmail.com>
2022-05-04 20:14:56 +0600

    urls

--- Web/login/views.py
+++ Web/login/views.py
@@ -6,14 +6,29 @@ from django.shortcuts import render
 def sing_in(request):
     return render(request, 'login/sing_in.html')
 
-
 def log_in(request):
     return render(request, 'login/log_in.html')
-
 
 def home(request):
     return render(request, 'login/about.html')
 
+def packages(request):
+    return render(request, 'login/packages.html')
+
+def blog_home(request):
+    return render(request, 'login/blog-home.html')
+
+def hotels(request):
+    return render(request, 'login/hotels.html')
+
+def insurance(request):
+    return render(request, 'login/insurance.html')
+
+def blog_single(request):
+    return render(request, 'login/blog-single.html')
+
+def contact(request):
+    return render(request, 'login/contact.html')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Page not found<h1>')
