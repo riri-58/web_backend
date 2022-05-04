faecde8bfc0d7d8be8655db8b0744b6027d6470f
Beam27 <nakosh0003@gmail.com>
2022-05-05 02:34:54 +0600

    aaaaa

--- Web/login/models.py
+++ Web/login/models.py
@@ -1,10 +1,12 @@
 from distutils.command.upload import upload
 from django.db import models
 
+
 class login(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to = "img/%Y/%m/%d/")
+    photo = models.ImageField(upload_to="img/%Y/%m/%d/")
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
