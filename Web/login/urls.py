65fb35f09821a8d7a6c39345e57c78f23b4f9550
200103032 <issadyk003@gmail.com>
2022-05-05 14:02:23 +0600

    ////

--- Web/login/urls.py
+++ Web/login/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('sign_in', views.sing_in, name='sign_in'),
     path('log_in', views.log_in, name='log_in'),
+<<<<<<< HEAD
     path('packages', views.packages, name='packages'),
     path('hotels', views.hotels, name='hotels'),
     path('insurance', views.insurance, name='insurance'),
@@ -12,5 +13,7 @@ urlpatterns = [
     path('blog_home', views.blog_home, name='blog_home'),
     path('contact', views.contact, name='contact'),
      path('index', views.index, name='index'),
+=======
+>>>>>>> parent of 008a891 (Merge branch 'main' of https://github.com/riri-58/web_backend)
 ]
 
--- Web/login/views.py
+++ Web/login/views.py
@@ -6,12 +6,15 @@ from django.shortcuts import render
 def sing_in(request):
     return render(request, 'login/sing_in.html')
 
+
 def log_in(request):
     return render(request, 'login/log_in.html')
+
 
 def home(request):
     return render(request, 'login/about.html')
 
+<<<<<<< HEAD
 def packages(request):
     return render(request, 'login/packages.html')
 
@@ -33,6 +36,8 @@ def contact(request):
 def index(request):
     return render(request, 'login/index.html')
 
+=======
+>>>>>>> parent of 008a891 (Merge branch 'main' of https://github.com/riri-58/web_backend)
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Page not found<h1>')
