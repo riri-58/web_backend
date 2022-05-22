75769e71330a6ea84620afa132cedcceaa560b8b
200103032 <issadyk003@gmail.com>
2022-05-22 19:01:16 +0600

    .....

--- Web/login/models.py
+++ Web/login/models.py
@@ -8,5 +8,23 @@ class login(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
 
+
+class comment(models.Model):
+    author = models.CharField(max_length=255)
+    comment = models.TextField(blank=True)
+    photo = models.ImageField(upload_to = "img/%Y/%m/%d/")  
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
 def __str__(self):
-    return self.title
+    return self.author
+
+
+class pacage(models.Model):
+    states = models.CharField(max_length=255)
+    day_2go = models.CharField(max_length=255)
+
+
+class basket(models.Model):
+    S_NAME = models.CharField(max_length=255)
